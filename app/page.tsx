b631220055a55b22b2e6613396a2eb7e4abc4d41
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-01 10:55:17 +0800

    feat (Home) : integrate all components to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,71 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import dayjs from "dayjs";
+import { Button, InputAdornment, TextField, Typography } from "@mui/material";
+
+import CustomDatePicker from "./components/CustomDatePicker";
+import Dropdown from "./components/Dropdown";
+import ResultTable from "./components/ResultTable";
 
 export default function Home() {
+  const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(dayjs());
+  const [desiredAmount, setDesiredAmount] = useState<number | ''>('');
+  const [dropdownValue, setDropdownValue] = useState<string | null>(null);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <Typography variant="h6" component="h1" sx={{ fontWeight: "bold" }}>
+          Quick MRSS Contribution Calculator
+        </Typography>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <Typography variant="body1" component="p">
+          This calculator helps you estimate your monthly contributions to the MRSS based on your desired annual amount.
+        </Typography>
+
+        {/* Input Area */}
+        <CustomDatePicker
+          value={selectedDate}
+          handleDateChange={(date) => {
+            setSelectedDate(date);
+          }}
+        />
+
+        <TextField fullWidth type="number"
+          value={desiredAmount}
+          label="Desired Amount in a Year"
+          slotProps={{
+            htmlInput: { min: 0 },
+            input: { startAdornment: <InputAdornment position="start">$</InputAdornment> },
+          }}
+          onChange={(event) => {
+            const value = event.target.value === '' ? '' : parseFloat(event.target.value);
+            setDesiredAmount(value);
+          }}
+        />
+
+        <Dropdown
+          value={dropdownValue}
+          onChange={(event, newValue) => {
+            setDropdownValue(newValue);
+          }}
+        />
+
+        <Button
+          variant="contained" sx={{ width: "30%", margin: "auto" }}
+          onClick={() => {
+            setSelectedDate(dayjs());
+            setDesiredAmount('');
+            setDropdownValue(null);
+          }}>Reset Inputs</Button>
+
+        {/* Output Area */}
+        <ResultTable
+          selectedDate={selectedDate}
+          desiredAmount={desiredAmount}
+          dropdownValue={dropdownValue}
+        />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
